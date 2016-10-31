a332534323c2c11ef16ba18d979caac927c6219f
wangzhenhua <wangzhenhua@huawei.com>
2016-10-31 11:20:36 +0800

    histogramEndpoints

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/FunctionRegistry.scala
@@ -235,6 +235,7 @@ object FunctionRegistry {
     expression[Remainder]("%"),
 
     // aggregate functions
+    expression[MapAggregate]("map_aggregate"),
     expression[HyperLogLogPlusPlus]("approx_count_distinct"),
     expression[Average]("avg"),
     expression[Corr]("corr"),
